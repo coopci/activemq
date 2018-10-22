9729707a0c22c4e18a78ddbeb6784423491b7fc1
Gu Bo <bgu@zuora.com>
2018-10-22 18:28:54 +0800

    add activemq-ide

---
+++ activemq-ide/src/main/java/org/apache/activemq/ide/Bootstrap.java
@@ -0,0 +1,8 @@
+package org.apache.activemq.ide;
+
+public class Bootstrap {
+    // just run with these application parameters: start xbean:file:activemq.xml
+    public static void main(String[] args) {
+        org.apache.activemq.console.Main.main(args);
+    }
+}
