dc1c3dda2ae2b1549c13cec21d6d5a960f107acf
Gu Bo <bgu@zuora.com>
2018-10-24 14:44:41 +0800

    add vm arguments.

--- activemq-ide/src/main/java/org/apache/activemq/ide/Bootstrap.java
+++ activemq-ide/src/main/java/org/apache/activemq/ide/Bootstrap.java
@@ -2,6 +2,7 @@ package org.apache.activemq.ide;
 
 public class Bootstrap {
     // just run with these application parameters: start xbean:file:activemq.xml
+    // -Xms64M -Xmx1G -Djava.util.logging.config.file=logging.properties -Djava.security.auth.login.config=/Users/bgu/Downloads/apache-activemq-5.14.5//conf/login.config -Dcom.sun.management.jmxremote -Djava.awt.headless=true -Djava.io.tmpdir=/Users/bgu/Downloads/apache-activemq-5.14.5//tmp -Dactivemq.classpath=/Users/bgu/Downloads/apache-activemq-5.14.5//conf:/Users/bgu/Downloads/apache-activemq-5.14.5//../lib/: -Dactivemq.home=/Users/bgu/Downloads/apache-activemq-5.14.5/ -Dactivemq.base=/Users/bgu/Downloads/apache-activemq-5.14.5/ -Dactivemq.conf=/Users/bgu/Downloads/apache-activemq-5.14.5//conf -Dactivemq.data=/Users/bgu/Downloads/apache-activemq-5.14.5//data
     public static void main(String[] args) {
         org.apache.activemq.console.Main.main(args);
     }
